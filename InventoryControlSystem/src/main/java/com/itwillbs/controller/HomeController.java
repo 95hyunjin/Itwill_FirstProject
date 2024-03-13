11fe4e545a5e56a411515565880cff3b3d880a5c
DOHYEON1582 <pondes@naver.com>
2024-03-13 17:54:03 +0900

    login.jsp

--- InventoryControlSystem/src/main/java/com/itwillbs/controller/HomeController.java
+++ InventoryControlSystem/src/main/java/com/itwillbs/controller/HomeController.java
@@ -22,18 +22,11 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public void registerGET() {
+		logger.debug(" registerGET() 호출");
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
 	}
 	
 }
